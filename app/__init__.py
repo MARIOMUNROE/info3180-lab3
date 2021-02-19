30dd0a7dfb583d2e949ce0d121b665fbdecb5672
MARIOMUNROE <mariomunroe7@gmail.com>
2021-02-19 13:50:05 -0500

    Second commit

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,19 @@
 from flask import Flask
+from flask_mail import Mail
+from .config import Config
+from flask_wtf.csrf import CSRFProtect
+
+csrf = CSRFProtect()
 
 app = Flask(__name__)
+app.config.from_object(Config)
+
+app.config['SECRET_KEY'] ='somesecretkey'
+app.config['MAIL_SERVER'] = 'smtp.mailtrap.io'
+app.config['MAIL PORT']= '465'
+app.config['MAIL_USERNAME']='dc65d868bd8845'
+app.config['MAIL_PASSWORD']='e5cdb9e774aae3'
+
+mail = Mail(app)
 from app import views
+csrf.init_app(app)
